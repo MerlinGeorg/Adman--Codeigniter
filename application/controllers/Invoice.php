ad440370eae4fa59a1e2d84a9c2cd6fbf5a4d4a1
anjaly <anjaly@flotillatechs.com>
2022-01-12 16:56:42 -0800

    add logo in outward invoice

--- application/controllers/Invoice.php
+++ application/controllers/Invoice.php
@@ -218,6 +218,7 @@ class Invoice extends Layout_Controller
 			$invo_list['n_asp'] = $this->invomodel->getasp();
 			$invo_list['n_package'] = $this->invomodel->gettpolicy();
 			$invo_list['involineedit'] = $this->invomodel->get_involine_edit($invo_id);
+			$invo_list['logo']  = $this->campmodel->getLogo($invo_id);
 			$this->load->view('invoice/invoice_edit', $invo_list);
 		} else {
 			$this->sess_out();
